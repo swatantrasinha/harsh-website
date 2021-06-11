b63f4fd32318cc6de5096306121580168259fe3b
“harshsahay0212” <harshsahay0212@gmail.com>
2021-06-12 00:11:29 +0530

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myrelationship() {
     // alert('humko koi click kiya');
     const para1 = document.getElementById('section1-para1');
-    para1.innerHTML = 'I am son of Swati and Piyush';
+    para1.innerHTML = 'I am elder brother of Darsh';
 }
