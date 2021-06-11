87567c6ac3824ba438761ea101f5bf8c6a147e9f
“harshsahay0212” <harshsahay0212@gmail.com>
2021-06-12 00:02:33 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function myrelationship() {
+    // alert('humko koi click kiya');
+    const para1 = document.getElementById('section1-para1');
+    para1.innerHTML = 'I am son of Swati and Piyush';
+}
