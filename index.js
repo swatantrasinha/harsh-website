bd44aae53fc83f33e1ad55de434d458a8b3038c7
“harshsahay0212” <harshsahay0212@gmail.com>
2021-06-12 00:14:22 +0530

    check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myrelationship() {
     // alert('humko koi click kiya');
     const para1 = document.getElementById('section1-para1');
-    para1.innerHTML = 'I am elder brother of Darsh';
+    para1.innerHTML = 'I am elder brother of Darsh... ';
 }
